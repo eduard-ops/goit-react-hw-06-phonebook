5705c06ad5b04465622c7a5368c548ee136cbb6e
eduard-ops <epustynnik@gmail.com>
2022-06-15 11:06:01 +0300

    index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from 'components/App';
 import './index.css';
-import store from 'redux/store';
+import { store } from 'redux/store';
 
 import { Provider } from 'react-redux';
 
