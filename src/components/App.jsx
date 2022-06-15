3992a7cb900661fb7b439fc76b0fcb3d47b83926
eduard-ops <epustynnik@gmail.com>
2022-06-15 11:05:39 +0300

    App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,11 +6,17 @@ import Container from './Container';
 
 import Filter from './Filter';
 
+import { useInfo } from 'redux/contacts/contactsReducer';
+
 export default function App() {
+  const { create } = useInfo();
+  const createContact = user => {
+    create(user);
+  };
   return (
     <Container>
       <h1>Phonebook</h1>
-      <Form />
+      <Form onSubmit={createContact} />
       <h2>Contacts</h2>
       <Filter />
       <Contacts />
